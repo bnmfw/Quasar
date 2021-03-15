1522ec277d2508d1f9ba9cf28bf5e49ae18f5b9d
bnmfw <bernardoborgessandoval@gmail.com>
2021-03-14 21:49:57 -0300

    b

--- atraso.py
+++ atraso.py
@@ -7,7 +7,7 @@ saidas = ["g1", "g2"]
 entradas = [Entrada("a",0),Entrada("b",0),Entrada("c",0),Entrada("d",0),Entrada("e",0)]
 vdd = 0.7
 circuito = "c17v0.txt"
-transicoes = [["rise","rise"],["rise","fall"],["fall","rise"],["fall","fall"]]
+transicoes = ["rise","fall"]
 maiorAtraso = 0
 simulacoesFeitas = 0
 
@@ -26,30 +26,32 @@ for entradaAnalisada in entradas:
                     flag += 1
 
             print("Nova transicao em analise")
-	    for transicao in transicoes:
-                #Etapa de verificacao de validacao
-                if transicao[0] == "rise": entradaAnalisada.sinal = 0
-                else: entradaAnalisada.sinal = 1
-                Definir_Fontes("fontes.txt", vdd, entradas)
-                analiseValida = Verificar_Validacao(circuito,"SETs.txt",entradaAnalisada.nome,transicao[0],saida,transicao[1],vdd)
-                simulacoesFeitas += 1
-                if not analiseValida: #Impede que uma analise invalida seja feita
-                    print("Analise invalida")
+            for transicaoNodo in transicoes:
+                for transicaoSaida in transicoes:
+                    #Etapa de verificacao de validacao
+                    if transicaoNodo == "rise": entradaAnalisada.sinal = 0
+                    else: entradaAnalisada.sinal = 1
+                    Definir_Fontes("fontes.txt", vdd, entradas)
+                    analiseValida = Verificar_Validacao(circuito,"SETs.txt",entradaAnalisada.nome,transicaoNodo,saida,transicaoSaida,vdd)
+                    simulacoesFeitas += 1
+                    if not analiseValida: #Impede que uma analise invalida seja feita
+                        print("Analise invalida")
+                        break
 
-                #Etapa de medicao de atraso
-                entradaAnalisada.sinal = transicao[0]
-                Escrever_Atraso(entradaAnalisada, saida, vdd, transicao[0], transicao[1])
-                Definir_Fontes("fontes.txt",vdd,entradas)
-                os.system("hspice " + circuito + " | grep \"atraso\" > texto.txt")
-                simulacoesFeitas += 1
-                atraso = Ler_Atraso()
-                print(atraso)
-                if atraso > entradaAnalisada.atraso[0]:
-                    entradaAnalisada.atraso[0] = atraso
-                    entradaAnalisada.atraso[1] = saida
-                if atraso != 0:
-                    print("Atraso encontrado")
-                    break
+                    #Etapa de medicao de atraso
+                    entradaAnalisada.sinal = transicaoNodo
+                    Escrever_Atraso(entradaAnalisada, saida, vdd, transicaoNodo, transicaoSaida)
+                    Definir_Fontes("fontes.txt",vdd,entradas)
+                    os.system("hspice " + circuito + " | grep \"atraso\" > texto.txt")
+                    simulacoesFeitas += 1
+                    atraso = Ler_Atraso()
+                    print(atraso)
+                    if atraso > entradaAnalisada.atraso[0]:
+                        entradaAnalisada.atraso[0] = atraso
+                        entradaAnalisada.atraso[1] = saida
+                    if atraso != 0:
+                        print("Atraso encontrado")
+                        break
     print("Fim da analise de atraso para entrada "+entradaAnalisada.nome+"")
     print(str(simulacoesFeitas)+" simulacoes feitas ate agora")
 
