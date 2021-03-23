9b4eb5e2985d09d0a0175eb45131b55ebd9f4bf1
bnmfw <bernardoborgessandoval@gmail.com>
2021-03-23 02:36:21 -0300

    alteracoes no SET

--- SETs.py
+++ SETs.py
@@ -32,79 +32,60 @@ if analiseManual:
     print("Corrente final: " + str(current))
 
 for nodo in nodos:
-
+    for saida in saidas:
+        nodo.relacoes.append([saida.nome])
     if analiseManual: break
     for nodo_saida in saidas:  # Determina a saida
+        for relacao in nodo.relacoes:
+            for i in range(4):
+                relacao.append(3000)  # correntes da relacao
+                relacao.append([])  # validacoes dessa corrente
+                #[g1,corrente,[vals],corrente,[vals],corrente,[vals],corrente,[vals]
 
-        for relacao in nodo.relacoes:  # Faz cada relacao com a saida
-            if relacao[0] == nodo_saida:  # encontrou a relacao
-                tipo = relacao[1]
-                if tipo == "nao":
-                    pass
-                else:
-                    relacao.append(3000)  # corrente rise da relacao
-                    relacao.append([])  # validacoes dessa corrente
-                    relacao.append(3000)  # corrente fall da relacao
-                    relacao.append([])  # validacoes dessa corrente
-                    if tipo == "com":  # Validacao completa
-                        relacao.append(3000)  # corrente rise da relacao
-                        relacao.append([])  # validacoes dessa corrente
-                        relacao.append(3000)  # corrente fall da relacao
-                        relacao.append([])  # validacoes dessa corrente
+            combinacoes = [["rise", "rise"], ["fall", "fall"], ["rise", "fall"], ["fall", "rise"]]
 
-                    combinacoes = []
-                    # identifica a relacao do nodo com a saida
-                    if tipo == "inv":
-                        combinacoes = [["rise", "fall"], ["fall", "rise"]]
-                    elif tipo == "dir":
-                        combinacoes = [["rise", "rise"], ["fall", "fall"]]
-                    elif tipo == "com":
-                        combinacoes = [["rise", "rise"], ["fall", "fall"], ["rise", "fall"], ["fall", "rise"]]
+            variaveis = 0
 
-                    variaveis = 0
+            # Descobre quantas variaveis na entrada
+            for val in nodo.validacao:
+                if val[0] == nodo_saida:
+                    validacao = list(val[1])
+                    for x in range(len(val[1])):
+                        if val[1][x] == "x": variaveis += 1
 
-                    # Descobre quantas variaveis na entrada
-                    for val in nodo.validacao:
-                        if val[0] == nodo_saida:
-                            validacao = list(val[1])
-                            for x in range(len(val[1])):
-                                if val[1][x] == "x": variaveis += 1
+            for k in range(2 ** variaveis):
+                binario = bin(k)
+                faltante = len(entradas) - len(binario) + 2
+                final = list(validacao)
+                flag = 0
+                binary = list()
+                # Transforma binario em uma lista de verdade e ajusta a validacao
+                for i in range(variaveis - (len(binario) - 2)): binary.append(0)
+                for i in range(len(binario) - 2): binary.append(int(binario[i + 2]))
+                for i in range(len(final)):
+                    if final[i] == "x":
+                        final[i] = binary[flag]
+                        flag += 1
 
-                    for k in range(2 ** variaveis):
-                        binario = bin(k)
-                        faltante = len(entradas) - len(binario) + 2
-                        final = list(validacao)
-                        flag = 0
-                        binary = list()
-                        # Transforma binario em uma lista de verdade e ajusta a validacao
-                        for i in range(variaveis - (len(binario) - 2)): binary.append(0)
-                        for i in range(len(binario) - 2): binary.append(int(binario[i + 2]))
-                        for i in range(len(final)):
-                            if final[i] == "x":
-                                final[i] = binary[flag]
-                                flag += 1
+                # Realiza a combinacao de rise e fall correta para validacao escolhida
+                for i in range(len(combinacoes)):
+                    print(nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], final)
+                    currente, simulacoesFeitas = Corrente(circuito, vdd, entradas, combinacoes[i][0], combinacoes[i][1], nodo.nome,
+                                     nodo_saida, final)
 
-                        # Realiza a combinacao de rise e fall correta para validacao escolhida
-                        for i in range(len(combinacoes)):
-                            print(nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], final)
-                            lista = Corrente(circuito, vdd, entradas, combinacoes[i][0], combinacoes[i][1], nodo.nome,
-                                             nodo_saida, final)
-                            current = lista[0]
-                            simulacoesFeitas += lista[1]
+                    if current < relacao[2 + 2 * i]:
+                        relacao[2 + 2 * i] = current
+                        relacao[3 + 2 * i] = [final]
+                    elif current == relacao[2 + 2 * i]:
+                        relacao[3 + 2 * i].append(final)
 
-                            if current < relacao[2 + 2 * i]:
-                                relacao[2 + 2 * i] = current
-                                relacao[3 + 2 * i] = [final]
-                            elif current == relacao[2 + 2 * i]:
-                                relacao[3 + 2 * i].append(final)
-
-                            if current < 1000:
-                                sets_validos.append(
-                                    [nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], current, final])
-                                break  # Se ja encontrou a combinacao valida praquela validacao nao tem pq repetir
-                            else:
-                                sets_invalidos.append(
-                                    [nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], current, final])
+                    if current < 1000:
+                        sets_validos.append(
+                            [nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], current, final])
+                        break  # Se ja encontrou a combinacao valida praquela validacao nao tem pq repetir
+                    else:
+                        sets_invalidos.append(
+                            [nodo.nome, nodo_saida, combinacoes[i][0], combinacoes[i][1], current, final])
 
 if not analiseManual:
     for sets in sets_validos: print(sets)
