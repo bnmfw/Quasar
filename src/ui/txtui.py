8ed139754a3311f17993305f0311f59c13a63455
Bernardo Borges <bnmfw@github.com>
2025-05-02 19:42:47 -0300

    txt ui error patched
    Signed-off-by: Bernardo Borges <bnmfw@github.com>

--- src/ui/txtui.py
+++ src/ui/txtui.py
@@ -293,7 +293,7 @@ class TXTUI:
             f"Simulator to be used ({'NGSPICE' if sim_config.runner_type == NGSpiceRunner else 'HSPICE'}): "
         )
         spice = spice.upper()
-        if spice not in {"NGSPICE", "HSPICE"}:
+        if spice not in {"NGSPICE", "HSPICE", ""}:
             raise ValueError("Spice Simulator not supported")
         if spice != "":
             inputs["spice"] = NGSpiceRunner if spice == "NGSPICE" else HSpiceRunner
